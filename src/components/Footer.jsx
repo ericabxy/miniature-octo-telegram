14b5d31fb608832d099cdc064b323ea933af3875
Eric Abides <ericxdu23@gmail.com>
2025-08-20 14:38:10 -0700

    footer component use footer tag

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,13 +1,13 @@
-import React from "react";
+import React from 'react';
 
 function Footer () {
     return (
-        <>
+        <footer>
           <p>
             <a href="https://github.com/ericabxy/miniature-octo-telegram/tree/main">source code</a>
           </p>
-        </>
-    );
+        </footer>
+    )
 }
 
 export default Footer;
